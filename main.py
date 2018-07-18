dcce09c8d31a4cb7b869b0f42ed6b75c85da9478
Mridul <mridulsahu01@gmail.com>
2018-07-18 12:16:51 +0530

    Checkpoint 3

--- main.py
+++ main.py
@@ -61,6 +61,80 @@ def create_notification(photo_name,
 
     return Notification(message=message, generation=generation)
 
+def create_thumbnail(photo_name):
+    filename = '/gs/{}/{}'.format(PHOTO_BUCKET, photo_name)
+    image = images.Image(filename=filename)
+    image.resize(width=180, height=200)
+    # The service accepts image data in the JPEG, PNG, WEBP, GIF (including animated GIF), BMP, TIFF and ICO formats.
+    # Transformed images can be returned in the JPEG, WEBP and PNG formats.
+    # If the input format and the output format are different,
+    # the service converts the input data to the output format before performing the transformation.
+    return image.execute_transforms(output_encoding=images.JPEG)
+
+def store_thumbnail_in_gcs(thumbnail_key, thumbnail):
+    write_retry_params = cloudstorage.RetryParams(
+        backoff_factor=1.1,
+        max_retry_period=15)
+    filename = '/{}/{}'.format(THUMBNAIL_BUCKET, thumbnail_key)
+    with cloudstorage.open(
+            filename, 'w', content_type='image/jpeg',
+            retry_params=write_retry_params) as filehandle:
+        filehandle.write(thumbnail)
+
+def get_labels(uri, photo_name):
+    service = googleapiclient.discovery.build('vision', 'v1')
+    labels = set()
+
+    # Label photo with its name, sans extension.
+    photo_details = os.path.splitext(photo_name) # cute-dog.jpg => ["cute-dog",".jpg"]
+    labels.add(photo_details[0])
+
+    service_request = service.images().annotate(body={
+        'requests': [{
+            'image': {
+                'source': {
+                    'imageUri': uri
+                }
+            },
+            'features': [{
+                'type': 'LABEL_DETECTION',
+                'maxResults': MAX_LABELS
+            }]
+        }]
+    })
+    response = service_request.execute()
+    labels_full = response['responses'][0].get('labelAnnotations')
+
+    ignore = set(['of', 'like', 'the', 'and', 'a', 'an', 'with'])
+
+    # Add labels to the labels list if they are not already in the list
+    # and are not in the ignore list.
+    if labels_full is not None:
+        for label in labels_full:
+            if label['description'] not in labels:
+                labels.add(label['description'])
+                # Split the label into individual words, also to be added to
+                # labels list if not already.
+                descriptors = label['description'].split()
+                for descript in descriptors:
+                    if descript not in labels and descript not in ignore:
+                        labels.add(descript)
+
+    return labels
+
+def get_original_url(photo_name, generation):
+    original_photo = 'https://storage.googleapis.com/' \
+        '{}/{}?generation={}'.format(
+            PHOTO_BUCKET,
+            photo_name,
+            generation)
+    return original_photo
+
+def get_thumbnail_serving_url(photo_name):
+    filename = '/gs/{}/{}'.format(THUMBNAIL_BUCKET, photo_name)
+    blob_key = blobstore.create_gs_key(filename)
+    return images.get_serving_url(blob_key)
+
 # Google Datastore NDB Client Library allows App Engine Python apps to connect to Cloud Datastore
 class Notification(ndb.Model):
     message = ndb.StringProperty()
@@ -88,7 +162,17 @@ class MainHandler(webapp2.RequestHandler):
 
 class PhotosHandler(webapp2.RequestHandler):
     def get(self):
-        template_values = {} #Nothing yet to pass to the template
+
+        thumbnail_references = ThumbnailReference.query().order(
+        -ThumbnailReference.date).fetch()
+        thumbnails = collections.OrderedDict()
+
+        for thumbnail_reference in thumbnail_references:
+            img_url = get_thumbnail_serving_url(thumbnail_reference.thumbnail_key)
+            thumbnails[img_url] = thumbnail_reference
+
+        template_values = {'thumbnails': thumbnails}
+
         template = jinja_environment.get_template('photos.html')
         self.response.write(template.render(template_values))
 
@@ -150,6 +234,21 @@ class ReceiveMessage(webapp2.RequestHandler):
 
         # Put notification in Cloud Datastore
         new_notification.put()
+
+        if event_type == 'OBJECT_FINALIZE':
+            thumbnail = create_thumbnail(photo_name)
+            store_thumbnail_in_gcs(thumbnail_key, thumbnail)
+
+            uri = 'gs://{}/{}'.format(PHOTO_BUCKET, photo_name)
+            labels = get_labels(uri, photo_name)
+            original_photo = get_original_url(photo_name, generation_number)
+
+            thumbnail_reference = ThumbnailReference(
+                                    thumbnail_name=photo_name,
+                                    thumbnail_key=thumbnail_key,
+                                    labels=list(labels),
+                                    original_photo=original_photo)
+            thumbnail_reference.put()
 
 
 app = webapp2.WSGIApplication([
